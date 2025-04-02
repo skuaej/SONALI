e3673de6f756b8205316e791fabbda2b4f1d8c27
ᴧʟᴘʜᴧ ʙᴧʙʏ <168528642+TEAMPURVI@users.noreply.github.com>
2025-04-02 15:16:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -131,7 +131,7 @@ confirmer = {}
 # ------------------------------------------------------------------------
 # ------------------------------------------------------------------------
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/eb31c20889c5fca9e153f.jpg, https://telegra.ph/file/62a4a5c93f78df95a7d61.jpg, https://telegra.ph/file/af1b9fb2c83194a37c44d.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/ksxja4.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://files.catbox.moe/9cevdg.jpg"
